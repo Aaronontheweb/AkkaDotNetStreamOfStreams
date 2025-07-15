d76ea2f88a264d1fd3d019a4da2ab99b20049908
Aaron Stannard <aaron@petabridge.com>
2025-07-15 16:41:35 -0500

    removed sample code; added data generators

---
+++ src/StreamOfStreams/Entities.cs
@@ -0,0 +1,52 @@
+﻿namespace StreamOfStreams;
+
+public interface IWithEntityId
+{
+    string EntityId { get; }
+}
+
+/// <summary>
+/// Some sort of entity that has an ID - not important.
+/// </summary>
+public sealed record Entity(string EntityId) : IWithEntityId;
+
+/// <summary>
+/// Some metadata for an entity.
+/// </summary>
+/// <param name="SomeScalar">An arbitrary scalar value</param>
+public sealed record EntityMetadata(double SomeScalar, double PreviousAverage);
+
+public sealed record DataPoint(double Value, DateTimeOffset Timestamp);
+
+public static class EntityData
+{
+    public static IEnumerable<Entity> Entities =>
+        Enumerable.Range(1, 100)
+            .Select(i => new Entity($"Entity-{i}"));
+    
+    public static EntityMetadata GenerateMetadata(Entity entity)
+    {
+        // Generate some metadata for the entity.
+        // This is just a placeholder for whatever logic you want to implement.
+        var random = Random.Shared;
+        return new EntityMetadata(
+            SomeScalar: random.NextDouble() * 100,
+            PreviousAverage: random.NextDouble() * 50);
+    }
+
+    public static IEnumerable<DataPoint> GenerateDataPoints(EntityMetadata entity)
+    {
+        // generate a random size of data points
+        var random = Random.Shared;
+        var count = random.Next(5, 50);
+        
+        // pick a deviation from the previous average
+        var deviation = random.NextDouble() * 10 - 5; // between -5 and +5
+        
+        // generate data points based on the previous average and deviation
+        return Enumerable.Range(1, count)
+            .Select(i => new DataPoint(
+                Value: entity.PreviousAverage + deviation + random.NextDouble() * 2 - 1, // small random variation
+                Timestamp: DateTimeOffset.UtcNow.AddMinutes(-i)));
+    }
+}
--- src/StreamOfStreams/Program.cs
+++ src/StreamOfStreams/Program.cs
@@ -6,21 +6,9 @@ var hostBuilder = new HostBuilder();
 
 hostBuilder.ConfigureServices((context, services) =>
 {
-    services.AddAkka("MyActorSystem", (builder, sp) =>
+    services.AddAkka("StreamOfStreams", (builder, sp) =>
     {
-        builder
-            .WithActors((system, registry, resolver) =>
-            {
-                var helloActor = system.ActorOf(Props.Create(() => new HelloActor()), "hello-actor");
-                registry.Register<HelloActor>(helloActor);
-            })
-            .WithActors((system, registry, resolver) =>
-            {
-                var timerActorProps =
-                    resolver.Props<TimerActor>(); // uses Msft.Ext.DI to inject reference to helloActor
-                var timerActor = system.ActorOf(timerActorProps, "timer-actor");
-                registry.Register<TimerActor>(timerActor);
-            });
+        
     });
 });
 
