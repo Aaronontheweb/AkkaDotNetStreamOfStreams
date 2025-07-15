00527383c3a9ff3e08cfee4c313a004e5c54b8c4
Aaron Stannard <aaron@petabridge.com>
2025-07-15 17:03:48 -0500

    renamed folders and projects (#1)
    * renamed folders and projects
    
    * removed sample code; added data generators
    
    * ignore .vscode
    
    * added S3 and DynamoDb actors

---
+++ src/StreamOfStreams/Program.cs
@@ -0,0 +1,35 @@
+﻿using Akka.Hosting;
+using StreamOfStreams;
+using Microsoft.Extensions.Hosting;
+using StreamOfStreams.Storage;
+
+var hostBuilder = new HostBuilder();
+
+hostBuilder.ConfigureServices((context, services) =>
+{
+    services.AddAkka("StreamOfStreams", (builder, sp) =>
+    {
+        builder
+            .WithActors((actorSystem, registry) =>
+            {
+                var dataPointActor = actorSystem.ActorOf(Props.Create(() => new DataPointActor()), "DataPointActor");
+                // Register the DataPointActor
+                registry.Register<DataPointActor>(dataPointActor);
+            })
+            .WithActors((system, registry) =>
+            {
+                var writerActor = system.ActorOf(Props.Create(() => new DynamoDbWriterActor()), "DynamoDbWriterActor");
+                // Register the DynamoDbWriterActor
+                registry.Register<DynamoDbWriterActor>(writerActor);
+                
+                // S3 actor
+                var s3WriterActor = system.ActorOf(Props.Create(() => new S3WriterActor()), "S3WriterActor");
+                // Register the S3WriterActor
+                registry.Register<S3WriterActor>(s3WriterActor);
+            });
+    });
+});
+
+var host = hostBuilder.Build();
+
+await host.RunAsync();
